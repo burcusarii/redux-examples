835a39a2fbd411f8ac317b28599d75774ce51fc8
burcu irem sarı <burcu.sari5334@gmail.com>
2023-12-02 14:38:31 +0300

    todo eklerken loading ve error gösterildi.

--- todo-app/client/src/components/Form.js
+++ todo-app/client/src/components/Form.js
@@ -1,12 +1,16 @@
+import React, { useEffect } from "react";
+import { message } from "antd";
 import { useState } from "react";
-import Loading from "./Loading";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodoAsync } from "../redux/todos/todosSlice";
+import Loading from "./Loading";
+
 function Form() {
   const [title, setTitle] = useState("");
-  const dispatch = useDispatch();
-
   const isLoading = useSelector((state) => state.todos.addNewTodoisLoading);
+  const newError = useSelector((state) => state.todos.addNewTodoError);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,24 +19,46 @@ function Form() {
     setTitle("");
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        disabled={isLoading}
-        className="new-todo"
-        placeholder="enter new todo"
-        autoFocus
-        value={title}
-        onChange={(e) => {
-          console.log(title);
+  // error mesajı
+  const [messageApi, contextHolder] = message.useMessage();
 
-          setTitle(e.target.value);
-        }}
-      />
-      <button className="btn-add" type="submit">
-        Add
-      </button>
-    </form>
+  const error = () => {
+    messageApi.open({
+      type: "error",
+      content: newError,
+    });
+  };
+  useEffect(() => {
+    if (newError) {
+      error();
+    }
+  }, [newError]);
+
+  return (
+    <div>
+      <div>
+        <div>{contextHolder}</div>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            disabled={isLoading}
+            className="new-todo"
+            placeholder="enter new todo"
+            autoFocus
+            value={title}
+            onChange={(e) => {
+              console.log(title);
+
+              setTitle(e.target.value);
+            }}
+          />
+          <button className="btn-add" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+      <div style={{ height: 15 }}>{isLoading && <Loading />}</div>
+    </div>
   );
 }
 
