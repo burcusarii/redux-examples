6882c3a3d32cf0d1ffe98547e4566b4a1aeee710
burcu irem sarı <burcu.sari5334@gmail.com>
2023-12-07 00:06:24 +0300

    react router ve redux kurularak proje temeli oluşturuldu.

---
+++ rick-and-morty-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Rick and Morty App
+      <a href="/deneme"> deneme sayfasına git</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ rick-and-morty-app/src/Deneme.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Deneme() {
+  return <div>deneme sayfası</div>;
+}
+
+export default Deneme;
