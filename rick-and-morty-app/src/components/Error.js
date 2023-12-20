2cdd17bdb8d98ad76e5cae51cfc504517b24ee9e
burcu irem sarı <burcu.sari5334@gmail.com>
2023-12-20 22:28:52 +0300

    error ve loading sayfaları eklendi.

---
+++ rick-and-morty-app/src/components/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Error({ message }) {
+  return <div style={{ margin: 10, color: "red" }}>Error : {message}</div>;
+}
+
+export default Error;
---
+++ rick-and-morty-app/src/components/Loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Loading() {
+  return <div style={{ margin: 10 }}>Loading</div>;
+}
+
+export default Loading;
