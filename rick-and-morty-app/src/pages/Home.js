bc00592d0c93ef209188e61bfcab59bf73d8c663
burcu irem sarı <burcu.sari5334@gmail.com>
2023-12-20 22:29:41 +0300

    Characters sayfasında sonraki sayfaların gösterilmesi gerçekleştirild.

--- rick-and-morty-app/src/pages/Home.js
+++ rick-and-morty-app/src/pages/Home.js
@@ -3,21 +3,43 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../redux/charactersSlice";
 import CharactersCard from "../components/CharactersCard";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 function Home() {
   const data = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const page = useSelector((state) => state.characters.page);
+  const hasNextPage = useSelector((state) => state.characters.hasNextPage);
+  const error = useSelector((state) => state.characters.error);
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
-  console.log(data);
+  if (error) {
+    return <Error message={error} />;
+  }
   return (
     <div>
-      <h1 className="char-title">Characters</h1>
+      <h1 className="page-title">Characters</h1>
       <div className="chars">
         {data.map((item) => {
           return <CharactersCard item={item} key={item.id} />;
         })}
+      </div>
+      <div style={{ textAlign: "center", padding: 35 }}>
+        {isLoading && <Loading />}
+
+        {!isLoading && hasNextPage && (
+          <button
+            className="next-page-btn"
+            onClick={() => dispatch(fetchCharacters(page))}
+          >
+            Next Page {page}
+          </button>
+        )}
       </div>
     </div>
   );
--- rick-and-morty-app/src/redux/charactersSlice.js
+++ rick-and-morty-app/src/redux/charactersSlice.js
@@ -2,22 +2,41 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 export const fetchCharacters = createAsyncThunk(
   "characters/getCharacters",
-  async () => {
-    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}`);
+  async (page) => {
+    const res = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/character/?page=${page}`
+    );
     return res.data.results;
   }
 );
-
 export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
+    error: null,
+    page: 1,
+    hasNextPage: true,
   },
 
   reducers: {},
   extraReducers(builder) {
+    builder.addCase(fetchCharacters.pending, (state, action) => {
+      state.isLoading = true;
+    });
     builder.addCase(fetchCharacters.fulfilled, (state, action) => {
-      state.items = action.payload;
+      state.items = [...state.items, ...action.payload];
+      state.isLoading = false;
+      state.page += 1;
+
+      if (action.payload.length < 20) {
+        state.hasNextPage = false;
+      }
+    });
+
+    builder.addCase(fetchCharacters.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
     });
   },
 });
