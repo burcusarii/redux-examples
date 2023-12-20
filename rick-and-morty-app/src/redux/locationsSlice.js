6b0d8a1f85fd592ef03f2c337478d613cfd7bc75
burcu irem sarı <burcu.sari5334@gmail.com>
2023-12-20 22:29:18 +0300

    locationSlice için extraReducers'lar yazıldı.

--- rick-and-morty-app/src/redux/locationsSlice.js
+++ rick-and-morty-app/src/redux/locationsSlice.js
@@ -13,12 +13,21 @@ export const locationsSlice = createSlice({
   name: "locations",
   initialState: {
     items: [],
+    isLoading: false,
   },
 
   reducer: {},
   extraReducers(builder) {
+    builder.addCase(fetchLocations.pending, (state, action) => {
+      state.isLoading = true;
+    });
     builder.addCase(fetchLocations.fulfilled, (state, action) => {
       state.items = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchLocations.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
     });
   },
 });
